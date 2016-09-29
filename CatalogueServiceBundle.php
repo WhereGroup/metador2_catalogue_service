cf5b9b355bcd6defb248f14bfb985dd8d7422358
A. Rashid Pour <info@rpour.de>
2016-09-29 14:11:25 +0200

    First commit

---
+++ CatalogueServiceBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Plugins\WhereGroup\CatalogueServiceBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class CatalogueServiceBundle
+ * @package Plugins\WhereGroup\CatalogueServiceBundle
+ */
+class CatalogueServiceBundle extends Bundle
+{
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# In den WhereGroup Plugin-Ordner wechseln. Erstellen wenn nicht vorhanden.
+cd src/Plugins/WhereGroup
+
+# Projekt clonen
+git clone git@repo.wheregroup.com:metador2/csw.git CatalogueServiceBundle 
+
+# Plugin nun in der Administration aktivieren
