b819f285a70a49395b4f5e5c8d82642993f2727f
Paul Schmidt <paul.schmidt@wheregroup.com>
2016-10-24 18:13:11 +0200

    fix filter handling add sortBy for GetRecords Operation

--- Component/Filter/AOperator.php
+++ Component/Filter/AOperator.php
@@ -1,13 +1,7 @@
 <?php
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 namespace Plugins\WhereGroup\CatalogueServiceBundle\Component\Filter;
 
-use Doctrine\ORM\Query\Expr;
 use Doctrine\ORM\QueryBuilder;
 use Plugins\WhereGroup\CatalogueServiceBundle\Component\CswException;
 
