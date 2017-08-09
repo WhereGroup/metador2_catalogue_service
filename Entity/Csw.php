17f34794717c692355e7822ba4594104dc22c060
Paul Schmidt <paul.schmidt@wheregroup.com>
2017-08-09 15:45:02 +0200

    add common csw attributes

--- Entity/Csw.php
+++ Entity/Csw.php
@@ -14,4 +14,136 @@ use Symfony\Component\Validator\Constraints as Assert;
  */
 class Csw
 {
+
+    /**
+     * @var string $slug slug
+     * @ORM\Id
+     * @ORM\Column(type="string")
+     */
+    protected $slug;
+
+    /**
+     * @var string $source source
+     * @ORM\Column(type="string", nullable=false)
+     */
+    protected $source;
+
+    /**
+     * @var string $title ServiceIdentification title
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $title;
+
+    /**
+     * @var string $abstract ServiceIdentification abstract
+     * @ORM\Column(type="text", nullable=true)
+     */
+    protected $abstract;
+
+    /**
+     * @var string $keywords ServiceIdentification keywords
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $keywords;
+
+    /**
+     * @var string $fees ServiceIdentification fees
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $fees;
+
+    /**
+     * @var string $accessConstraints ServiceIdentification accessConstraints
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $accessConstraints;
+
+    /**
+     * @var string $accessConstraints ServiceIdentification accessConstraints
+     * @ORM\Column(type="string", nullable=false)
+     */
+    protected $providerName;
+
+    /**
+     * @var string $accessConstraints ServiceIdentification accessConstraints
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $providerSite;
+
+    /**
+     * @var string $accessConstraints ServiceIdentification accessConstraints
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $serviceContact;
+
+    /**
+     * @var string $accessConstraints ServiceIdentification accessConstraints
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $individualName;
+
+    /**
+     * @var string $accessConstraints ServiceIdentification accessConstraints
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $positionName;
+
+    /**
+     * @var string $accessConstraints ServiceIdentification accessConstraints
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $phoneVoice;
+
+    /**
+     * @var string $accessConstraints ServiceIdentification accessConstraints
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $phoneFacsimile;
+
+    /**
+     * @var string $accessConstraints ServiceIdentification accessConstraints
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $deliveryPoint;
+
+    /**
+     * @var string $accessConstraints ServiceIdentification accessConstraints
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $city;
+
+    /**
+     * @var string $accessConstraints ServiceIdentification accessConstraints
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $administrativeArea;
+
+    /**
+     * @var string $accessConstraints ServiceIdentification accessConstraints
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $postalCode;
+
+    /**
+     * @var string $accessConstraints ServiceIdentification accessConstraints
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $country;
+
+    /**
+     * @var string $accessConstraints ServiceIdentification accessConstraints
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $electronicMailAddress;
+
+    /**
+     * @var string $accessConstraints ServiceIdentification accessConstraints
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $onlineResourse;
+
+
+
+
+
 }
