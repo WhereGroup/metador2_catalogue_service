39a748a933c86a09a4f10a03e2b141167b831541
Paul Schmidt <paul.schmidt@wheregroup.com>
2017-08-10 16:03:39 +0200

    add functions save, remove

--- Entity/CswRepository.php
+++ Entity/CswRepository.php
@@ -10,5 +10,41 @@ use Doctrine\ORM\EntityRepository;
  */
 class CswRepository extends EntityRepository
 {
+    /**
+     * @return mixed
+     */
+    public function count()
+    {
+        return $this
+            ->getEntityManager()
+            ->getRepository("CatalogueServiceBundle:Csw")
+            ->createQueryBuilder('u')
+            ->select('count(u.slug)')
+            ->getQuery()
+            ->getSingleScalarResult();
+    }
+    /**
+     * @param $entity
+     * @return $this
+     */
+    public function save($entity)
+    {
+        $this->getEntityManager()->persist($entity);
+        $this->getEntityManager()->flush();
+
+        return $this;
+    }
+
+    /**
+     * @param $entity
+     * @return $this
+     */
+    public function remove($entity)
+    {
+        $this->getEntityManager()->remove($entity);
+        $this->getEntityManager()->flush();
+
+        return $this;
+    }
 
 }
